48d1a83b99467376a102a0ad95b02106f15da07c
Pawel Konior <pawel.konior@gmail.com>
2023-11-09 20:30:01 +0100

    show message when flashcards not found

--- src/components/learning/LearningWrapper.jsx
+++ src/components/learning/LearningWrapper.jsx
@@ -30,10 +30,14 @@ function LearningWrapper() {
   return (
     <div>
       Learning: {deckId} {deck.name} {deck.description}
-      <FlashCard
-        question={flashcards[0].question}
-        answer={flashcards[0].answer}
-      />
+      {flashcards.length ? (
+        <FlashCard
+          question={flashcards[0].question}
+          answer={flashcards[0].answer}
+        />
+      ) : (
+        <p>Flashcards not found</p>
+      )}
     </div>
   );
 }
